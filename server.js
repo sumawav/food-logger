e6ee37de4ed82cbd973393c969548917b70495bf
Souma <mondal.souma@gmail.com>
2022-06-19 12:41:59 -0500

    adds express validator and basic user registration validation

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const connectDB = require('./config/db');
 
 connectDB();
 
+// Init Middleware
+app.use(express.json({ extended: false }));
+
 app.get('/', (req, res) => res.send('API Running'));
 
 // define routes
