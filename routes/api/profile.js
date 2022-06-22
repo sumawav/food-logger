cadbe44299a1a506d865ad63afb4fdd48282f6ea
Souma <mondal.souma@gmail.com>
2022-06-22 16:25:47 -0500

    adds route to get current profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,9 +1,31 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth');
+// const { check, validationResult } = require('express-validator');
 
-// @route   GET api/profile
-// @desc    test route
-// @access  Public
-router.get('/', (req, res) => res.send('Profile Route'));
+const Profile = require('../../models/Profile');
+const User = require('../../models/User');
+
+// @route   GET api/profile/me
+// @desc    Get current user's profile
+// @access  Private
+router.get('/me', auth, async (req, res) => {
+    try {
+        const profile = await Profile.findOne({ user: req.user.id }).populate(
+            'user',
+            ['name']
+        );
+
+        if (!profile) {
+            return res
+                .status(400)
+                .json({ msg: 'There is no profile for that user' });
+        }
+        res.json(profile);
+    } catch (err) {
+        console.log(err.message);
+        res.status(500).send('Server Error');
+    }
+});
 
 module.exports = router;
