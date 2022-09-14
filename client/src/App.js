204ad71be6498ecea78707e4337f12a49b5508b7
Souma <mondal.souma@gmail.com>
2022-09-14 11:33:23 -0500

    adds basic react router

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,26 @@
 import * as React from 'react';
-import { BrowserRouter as Router, ROute, Switch } from 'react-router-dom';
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+    Switch,
+} from 'react-router-dom';
 import { StyledEngineProvider } from '@mui/material/styles';
 import Navbar from './components/layout/Navbar';
 import Dashboard from './components/layout/Dashboard';
 
 function App() {
     return (
-        <React.StrictMode>
-            <StyledEngineProvider injectFirst>
-                <Navbar />
-                <Dashboard />
-            </StyledEngineProvider>
-        </React.StrictMode>
+        <Router>
+            <React.StrictMode>
+                <StyledEngineProvider injectFirst>
+                    <Navbar />
+                    <Routes>
+                        <Route exact path="/" element={<Dashboard />} />
+                    </Routes>
+                </StyledEngineProvider>
+            </React.StrictMode>
+        </Router>
     );
 }
 
